26c033ee169b8115caad623029d858653535a386
CarloCapuz <36867035+CarloCapuz@users.noreply.github.com>
2018-03-08 22:11:25 -0500

    Update index.js

--- jsguess/index.js
+++ jsguess/index.js
@@ -1,1 +1,16 @@
+function runSubmit() {
+	guess = prompt("Player 1, enter a positive number: ")
+	number = prompt("What's your guess?");
 
+for (count=1; count<20; count++) {
+		if (number > guess) {
+    	alert("Too high!");
+		} else if (number < guess) {
+    	alert("Too low!");
+		} else {
+    	alert("Congratulations! It took you " + count + " tries.");
+			break;
+		}
+		count++;
+	}
+}
